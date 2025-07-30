79f22c9606b86384facf1f27765f0ee523693332
anreymon <anreymon@gmail.com>
2025-07-30 15:41:41 +0200

    exercise 08 done

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, num) {
+    for (let i = 1; i < arguments.length; i++) {
+        arr = arr.filter(x => x !== arguments[i]);
+    }
+    return arr;
 };
 
 // Do not edit below this line
