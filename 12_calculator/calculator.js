67d69142bae3cc254d821cebfda6c54843232f27
anreymon <anreymon@gmail.com>
2025-08-01 15:14:14 +0200

    exercise 12 done

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(nb1, nb2) {
+	return nb1 + nb2;
 };
 
-const subtract = function() {
-	
+const subtract = function(nb1, nb2) {
+  return nb1 - nb2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce(add, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  if (arr.length == 0) return 0;
+  return arr.reduce((a,b) => a*b, 1);
 };
 
-const power = function() {
-	
+const power = function(nb1, nb2) {
+	return nb1 ** nb2;
 };
 
-const factorial = function() {
-	
+const factorial = function(nb) {
+	if (nb > 0) return nb * factorial(nb-1);
+  else if (nb === 0) return 1;
+  else return "ERROR";
 };
 
 // Do not edit below this line
