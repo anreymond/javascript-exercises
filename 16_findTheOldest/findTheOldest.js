757a9f53b04713d2c0b213067574d4f3591f1d55
anreymon <anreymon@gmail.com>
2025-08-02 20:18:03 +0200

    exercise 16 done

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((prev, curr) => {
+        let date = new Date();
+        let prevAge = "yearOfDeath" in prev ? prev.yearOfDeath-prev.yearOfBirth : date.getFullYear()-prev.yearOfBirth;
+        let currAge = "yearOfDeath" in curr ? curr.yearOfDeath-curr.yearOfBirth : date.getFullYear()-curr.yearOfBirth;
+        return prevAge > currAge ? prev : curr;
+    });
 };
 
 // Do not edit below this line
