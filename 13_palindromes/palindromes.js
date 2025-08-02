a089f557cdf054ae44e8f41335f806c48989f429
anreymon <anreymon@gmail.com>
2025-08-02 19:36:38 +0200

    exercise 13 done

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const alphanumeric = "abcdefghijklmnopqrstuvwxyz0123456789";
+    str = str.toLowerCase().split("").filter(c => alphanumeric.includes(c)).join("");
+    let reverse = str.split("").reverse().join("");
+    return str === reverse;
 };
 
 // Do not edit below this line
