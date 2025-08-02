224092d37a4c6280716f37ed2345670970723650
anreymon <anreymon@gmail.com>
2025-08-02 19:41:33 +0200

    exercise 14 done

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if (index < 0) return "OOPS";
+    let fib = [0, 1];
+    for (let i = 2; i <= index; i++) {
+        fib[i] = fib[i-1] + fib[i-2];
+    }
+    return fib[index];
 };
 
 // Do not edit below this line
